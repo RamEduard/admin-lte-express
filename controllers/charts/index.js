2798d3232d81046e006d0450249e3118ccc689fc
RamEduard <ramon.calle.88@gmail.com>
2016-03-02 20:07:37 -0430

    Charts controller added.

---
+++ controllers/charts/index.js
@@ -0,0 +1,22 @@
+exports.prefix = '/charts';
+
+exports.chartjs = function(request, response) {
+  response.render("charts/chart-js", {
+    title: "ChartJS"
+  })
+};
+exports.morris = function(request, response) {
+  response.render("charts/morris", {
+    title: "Morris"
+  })
+};
+exports.flot = function(request, response) {
+  response.render("charts/flot", {
+    title: "Flot"
+  })
+};
+exports.inlineCharts = function(request, response) {
+  response.render("charts/inline-charts", {
+    title: "Inline Charts"
+  })
+};
