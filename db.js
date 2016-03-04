d22ea24064f244a6501c6b9ac56cdadd2e7eb26c
RamEduard <ramon.calle.88@gmail.com>
2016-03-04 16:15:38 -0430

    Authorization integrated to DB module.

--- db.js
+++ db.js
@@ -1,23 +1,62 @@
 var mongoose = require('mongoose'),
-	Schema   = mongoose.Schema
+    Schema   = mongoose.Schema;
 
-//mongoose.connect('mongodb://localhost:27017/admin-lte-express')
+// Uncomment for use mongo database
+//mongoose.connect('mongodb://localhost:27017/admin-lte-express');
 
 // Users Model
 var usersSchema = new Schema({
-	id: Number,
-	name: String,
-	password: String
-}/*, {collection:'users'}*/);
+  id: Number,
+  username: String,
+  password: String,
+  displayName: String,
+  emails: Array
+}, {collection:'users'});
 
-module.exports.User = User = mongoose.model('User', usersSchema);
+var User = exports.User = mongoose.model('User', usersSchema);
 
-module.exports.getUsers = function(callback) {
-	User.find().exec(function(err, users) {
-		if (err) throw err
-
-		callback(users)
-	})
+exports.getUsers = function(cb) {
+  User.find().exec(function(err, results) {
+    cb(err, results);
+  });
 };
 
-module.exports.users = users = []
+var users = exports.users = [
+  new User({ 
+    id: 1, 
+    username: 'admin', 
+    password: 'admin', 
+    displayName: 'Jack', 
+    emails: [ { value: 'jack@example.com' } ] 
+  })]
+
+// add users from collection mongo
+this.getUsers(function(err, results) {
+  if (err) throw err;
+  results.forEach(function(user) {
+    users.push(user);
+  });
+});
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    var idx = id - 1;
+    if (users[idx]) {
+      cb(null, users[idx]);
+    } else {
+      cb(new Error('User ' + id + ' does not exist'));
+    }
+  });
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = users.length; i < len; i++) {
+      var user = users[i];
+      if (user.username === username) {
+        return cb(null, user);
+      }
+    }
+    return cb(null, null);
+  });
+}
